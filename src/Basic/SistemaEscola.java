6321e57c1eca125a3038ecc3d2ebc949852dc028
PauloJunior <paulojunior.10@outlook.com>
2023-02-18 16:51:46 -0300

    mais uma aula

--- src/Basic/SistemaEscola.java
+++ src/Basic/SistemaEscola.java
@@ -1,9 +1,28 @@
 package Basic;
 
+import java.util.Scanner;
+
 public class SistemaEscola {
 
 	public static void main(String[] args) {
-		System.out.println("Olá mundo");
+		Scanner scanner = new Scanner(System.in);
+		
+		double somadorNotas = 0;
+		int somadorNumeroNotas = 0;
+		double nota = 1;
+		
+		while(nota != 0) {
+			System.out.println("Insira uma nota: ");
+			nota = scanner.nextDouble();
+			if(nota !=0) {
+				somadorNotas += nota;
+				somadorNumeroNotas += 1;
+			}
+		}
+		
+		double media = somadorNotas / somadorNumeroNotas;
+		
+		System.out.println("Media: " + media);
 	}
 
 }
